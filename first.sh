d3c877f7942d1f8ba24bbd6db45bd52930d991d2
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2024-12-08 22:56:47 +0500

    input using shell

---
+++ first.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+FIRST_NAME=Aboubakar
+LAST_NAME=Arisar
+echo Hello $FIRST_NAME $LAST_NAME
---
+++ input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo what is your name?
+read FIRST_NAME
+echo what is your last name?
+read LAST_NAME
+
+echo $FIRST_NAME $LAST_NAME
+
